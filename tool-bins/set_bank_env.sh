e09fd2f91daafca8280ba2576fa0cf30853b8276
ugyentring <tsheringugyen483@gmail.com>
2025-06-11 01:01:55 +0000

    added

--- tool-bins/set_bank_env.sh
+++ tool-bins/set_bank_env.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
+
+# Basic org context
 export ORG_CONTEXT=bank
 export ORG_NAME=Bank
 export CORE_PEER_LOCALMSPID=BankMSP
-# Logging specifications
+
+# Logging
 export FABRIC_LOGGING_SPEC=INFO
-# Location of the core.yaml
+
+# Core config
 export FABRIC_CFG_PATH=/workspaces/dnetwork/config/bank
-# Address of the peer
 export CORE_PEER_ADDRESS=bank.dmarket.com:7051
-# Local MSP for the admin - Commands need to be executed as org admin
-export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/dmarket.com/peers/bank/msp
-# Address of the orderer
+
+# Admin MSP path (pointing to Admin user MSP)
+export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/bank.dmarket.com/users/Admin@bank.dmarket.com/msp
+
+# Orderer
 export ORDERER_ADDRESS=orderer.dmarket.com:7050
 export CORE_PEER_TLS_ENABLED=false
-#### Chaincode related properties
+
+# Chaincode
 export CC_NAME="cakechain"
 export CC_PATH="./chaincodes/cakechain/"
 export CC_CHANNEL_ID="dmarketchannel"
 export CC_LANGUAGE="golang"
 
-# Properties of Chaincode
+# Chaincode versioning
 export INTERNAL_DEV_VERSION="1.0"
 export CC_VERSION="1.1"
 export CC2_PACKAGE_FOLDER="./chaincodes/packages/"
 export CC2_SEQUENCE=1
 export CC2_INIT_REQUIRED="--init-required"
 
-# Create the package with this name
-export CC_PACKAGE_FILE="$CC2_PACKAGE_FOLDER$CC_NAME.$CC_VERSION-$INTERNAL_DEV_VERSION.tar.gz"
-# Extracts the package ID for the installed chaincode
-export CC_LABEL="$CC_NAME.$CC_VERSION-$INTERNAL_DEV_VERSION"
+# Chaincode package
+export CC_PACKAGE_FILE="${CC2_PACKAGE_FOLDER}${CC_NAME}.${CC_VERSION}-${INTERNAL_DEV_VERSION}.tar.gz"
+export CC_LABEL="${CC_NAME}.${CC_VERSION}-${INTERNAL_DEV_VERSION}"
 
-# peer lifecycle chaincode package  $CC_PACKAGE_FILE -p $CC_PATH --label $CC_LABEL
+# Test command (optional)
 peer channel list
--- tool-bins/set_peer_env.sh
+++ tool-bins/set_peer_env.sh
@@ -1,63 +1,15 @@
-# #!/bin/bash
-# #Sets the context for native peer commands
-
-
-# function usage {
-#     echo "Usage:             . ./set_peer_env.sh  ORG_NAME"
-#     echo "           Sets the organization context for native peer execution"
-# }
-
-
-# if [ "$1" == "" ]; then
-#     usage
-#     exit
-# fi
-# export ORG_CONTEXT=$1
-# MSP_ID="$(tr '[:lower:]' '[:upper:]' <<< ${ORG_CONTEXT:0:1})${ORG_CONTEXT:1}"
-# export ORG_NAME=$MSP_ID
-
-
-# # Added this Oct 22
-# export CORE_PEER_LOCALMSPID=$ORG_NAME"MSP"
-
-
-# # Logging specifications
-# export FABRIC_LOGGING_SPEC=INFO
-
-
-# # Location of the core.yaml
-# export FABRIC_CFG_PATH=/workspaces/hfl-network1/config/cit
-
-
-# # Address of the peer
-# export CORE_PEER_ADDRESS=cit.$1.edu:7051
-
-
-# # Local MSP for the admin - Commands need to be executed as org admin
-# export CORE_PEER_MSPCONFIGPATH=/workspaces/hfl-network1/config/crypto-config/peerOrganizations/$1.edu/users/Admin@$1.edu/msp
-
-
-# # Address of the orderer
-# export ORDERER_ADDRESS=orderer.confectionary.com:7050
-
-# export CORE_PEER_TLS_ENABLED=false
-
-
-
-
-# My version of the code for the set_peer_env.sh wrote on 7th June 2025
 #!/bin/bash
 # Sets the context for native peer commands
 
 function usage {
-    echo "Usage:             . ./set_peer_env.sh  ORG_NAME"
+    echo "Usage:             . ./set_peer_env.sh ORG_NAME"
     echo "           Sets the organization context for native peer execution"
     echo "           ORG_NAME must be one of: bank, buyer, seller"
 }
 
 if [ "$1" == "" ]; then
     usage
-    exit 1
+    return 1
 fi
 
 ORG_CONTEXT=$1
@@ -68,26 +20,26 @@ case "$ORG_CONTEXT" in
         export CORE_PEER_LOCALMSPID=BankMSP
         export FABRIC_CFG_PATH=/workspaces/dnetwork/config/bank
         export CORE_PEER_ADDRESS=bank.dmarket.com:7051
-        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/dmarket.com/peers/bank/msp
+        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/bank.dmarket.com/users/Admin@bank.dmarket.com/msp
         ;;
     buyer)
         export ORG_NAME=Buyer
         export CORE_PEER_LOCALMSPID=BuyerMSP
         export FABRIC_CFG_PATH=/workspaces/dnetwork/config/buyer
         export CORE_PEER_ADDRESS=buyer.dmarket.com:7051
-        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/dmarket.com/peers/buyer/msp
+        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/buyer.dmarket.com/users/Admin@buyer.dmarket.com/msp
         ;;
     seller)
         export ORG_NAME=Seller
         export CORE_PEER_LOCALMSPID=SellerMSP
         export FABRIC_CFG_PATH=/workspaces/dnetwork/config/seller
         export CORE_PEER_ADDRESS=seller.dmarket.com:7051
-        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/dmarket.com/peers/seller/msp
+        export CORE_PEER_MSPCONFIGPATH=/workspaces/dnetwork/config/crypto-config/peerOrganizations/seller.dmarket.com/users/Admin@seller.dmarket.com/msp
         ;;
     *)
         echo "Unknown organization: $ORG_CONTEXT"
         usage
-        exit 1
+        return 1
         ;;
 esac
 
